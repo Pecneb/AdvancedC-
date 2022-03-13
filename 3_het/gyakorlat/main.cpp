c27e3f396aeb5e1b6a0e0e772b67655bcfae2727
Pecneb <ecneb2000@gmail.com>
2022-03-13 11:15:39 +0100

    3.het gyakorlat feladat done

--- 3_het/gyakorlat/main.cpp
+++ 3_het/gyakorlat/main.cpp
@@ -1,47 +1,85 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <algorithm>
 #include <unordered_map>
 #include <string>
 #include <sstream>
 
-typedef std::pair<std::string, size_t> NumOfWord; // The count of the word.
+typedef std::pair<size_t, std::string> IntStringPairs; /* count x word */
 typedef std::vector<std::string> StringVector;
-typedef std::unordered_map<std::string, size_t> UmapNumOfWord;
-typedef std::vector<NumOfWord>  NumOfWordVector;
+typedef std::unordered_map<std::string, size_t> UmapIntStringPairs; /* word x count */
+typedef std::vector<IntStringPairs> VectorOfIntStringPairs; /* Vector storing IntStringPairs pairs (pair<size_t, std::string) */
 
 std::vector<std::string> words(const std::string& sentence) {
+	
+	/* Waits a sentence or some string as input.
+	 * It splits the sentence at spaces and
+	 * produce a vector of words */
 
-  StringVector wordVec;
+	StringVector wordVec;
 
-  std::istringstream ss(sentence);
+	std::istringstream ss(sentence);
 
-  std::string token;
+	std::string token; // a single word from the sentence
 
-  while( std::getline( ss, token, ' ') ) wordVec.push_back(token);
+	while( std::getline( ss, token, ' ') ) wordVec.push_back(token);
 
-  return wordVec;
+	return wordVec;
 }
 
-UmapNumOfWord count(StringVector v) { 
+UmapIntStringPairs count(StringVector v) { 
 
-  // Every every word in the sentence gets a number, how many times it is in the sentence.
+	/* Waits a vector of words as input.
+	 * It counts how many times a word
+	 * appears in the sentence and
+	 * produces an unordered_map
+	 * with pairs<string, size_t>. */
 
-  UmapNumOfWord ret;
+	UmapIntStringPairs ret;
 
-  for (auto i : v) {
+	for (auto word : v) {
 
-    if (ret.find(i) == ret.end()) ret[i] = 1;
+		/* if word not found in map, initialize a <key, value> pair with key = word, value = 1 */
+		
+		if (ret.find(word) == ret.end()) ret[word] = 1; 
 
-    else ret[i]++;
+		/* if word is found, increment the value by 1 */
 
-  }
+		else ret[word]++;
 
-  return ret;
+	}
+
+	return ret;
 }
 
-//TODO: reverse() function
-//TODO: sortVectorOfIntStringPairs() function
+VectorOfIntStringPairs reverse(UmapIntStringPairs um) {
+
+    /* Swap the key, value of unordered_map<std::string, size_t> um
+     * and put them in a vector<pair<size_t, std::string>> */
+
+	VectorOfIntStringPairs ret;
+
+	for (auto k : um) {
+
+        IntStringPairs p = std::make_pair(k.second, k.first);
+        ret.push_back(p);
+
+    }
+
+    return ret;
+}
+
+VectorOfIntStringPairs sortVectorOfIntStringPairs(VectorOfIntStringPairs&& v) {
+
+    /* Sorts the input vector<pair<size_t, std::string>> based on key to descending order. */
+
+    VectorOfIntStringPairs ret(std::move(v));
+
+    std::sort(ret.begin(), ret.end(), [](IntStringPairs paira, IntStringPairs pairb) -> bool { return paira.first > pairb.first; });
+
+    return ret;
+} 
 
 // Elvart kimenet:
 // 3: very
@@ -54,13 +92,13 @@ UmapNumOfWord count(StringVector v) {
 
 int main() {
 
-  VectorOfIntStringPairs voisp = sortVectorOfIntStringPairs( reverse( count( words("this string is a very very very short string this one!"))));
+	VectorOfIntStringPairs voisp = sortVectorOfIntStringPairs( reverse( count( words("this string is a very very very short string this one!"))));
 
-  for (auto& v : voisp) {
+	for (auto& v : voisp) {
 
-    std::cout << v.first << ": " << v.second << std::endl;
+		std::cout << v.first << ": " << v.second << std::endl;
 
-  }
+	}
 
-  return 0;
+	return 0;
 }
