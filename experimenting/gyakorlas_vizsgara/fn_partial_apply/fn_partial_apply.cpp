87c2617b784a658c997ee24ab585374fc7ddfe50
Pecneb <ecneb2000@gmail.com>
2022-05-28 20:08:35 +0200

    gyakorlas feladatok update

--- experimenting/gyakorlas_vizsgara/fn_partial_apply/fn_partial_apply.cpp
+++ experimenting/gyakorlas_vizsgara/fn_partial_apply/fn_partial_apply.cpp
@@ -4,20 +4,22 @@
 
 using namespace std;
 
+/* Pretty much a self made bind2nd() function */
 template < typename Function, typename SecondArgType >
 
 class partial_apply_2nd {
 
 private:
-  
-  Function function;
 
-  SecondArgType second;
+  Function function; /* function to execute */
+
+  SecondArgType second; /* second arg of function */
 
 public:
 
-  partial_apply_2nd(Function f, SecondArgType s) : function(f), second(s) {}
+  partial_apply_2nd(Function f, SecondArgType s) : function(f), second(s) {} /* construct the object with a function, and the second argument of the function */
 
+  /* functor operator -> call constructed object with the first arg of the function */
   template < typename FirstArgType >
 
   decltype( auto ) operator()( FirstArgType&& first ) {
@@ -42,6 +44,14 @@ int main() {
 
   cout << "17 is greater than 18? " << ( greaterThan18( 17 ) ? "Yes" : "No" ) << '\n';
 
+  auto greaterThan10 = bind( greater<int>(), placeholders::_1, 10);
+
+  cout << "15 is greater than 10? " << ( greaterThan10(15) ? "Yes" : "No" ) << '\n';
+
+  auto thousandTimesSomething = bind( function< int( int, int ) >( [] ( int x, int m) -> int { return x*m; } ), placeholders::_1, 1000);
+
+  cout << "This is 1000 x 1000 = " << thousandTimesSomething(1000) << '\n'; 
+ 
   return 0;
 
 }
